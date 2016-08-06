4c5a0088ab878659e1aac0ae2b79c77f9e3b23a1
madou <laheen@gmail.com>
2016-08-06 23:00:44 +1000

    Adds avatar

---
+++ src/common/components/Avatar/index.js
@@ -0,0 +1,26 @@
+import { PropTypes } from 'react';
+import styles from './styles.less';
+import classnames from 'classnames/bind';
+const cx = classnames.bind(styles);
+
+const Avatar = ({ alias, img, size }) => (
+  <div title={alias} className={styles.root}>
+    <img
+      alt={alias}
+      className={cx('image', size)}
+      src={img}
+    />
+
+    <h2 className={styles.name}>
+      <strong>{alias || 'loading...'}</strong>
+    </h2>
+  </div>
+);
+
+Avatar.propTypes = {
+  alias: PropTypes.string,
+  size: PropTypes.string,
+  img: PropTypes.string,
+};
+
+export default Avatar;
--- src/features/User/index.js
+++ src/features/User/index.js
@@ -1,8 +1,10 @@
 import styles from './styles.less';
 import CharactersList from 'common/components/CharactersList';
+import Avatar from 'common/components/Avatar';
 
 export default () => (
   <div className={styles.container}>
+    <Avatar />
     <CharactersList />
   </div>
 );
