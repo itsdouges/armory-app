7cc1ee732fbb755396e81f676caf399a5d091495
madou <laheen@gmail.com>
2016-08-20 17:58:18 +1000

    Adds deploy to packagejson, fixes white space in deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@
 # fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
- echo "Deploying to preview from development."
- sh scripts/robottxt-gen.sh disallow
+  echo "Deploying to preview from development."
+  sh scripts/robottxt-gen.sh disallow
   npm run deploy -- --env PREVIEW;
 fi
