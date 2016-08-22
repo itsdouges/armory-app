99bbd2f381b80158c68b87b5b87ec4a510ef318f
madou <laheen@gmail.com>
2016-08-22 20:41:37 +1000

    Adds icon to gold component

--- src/common/components/Tooltip/Gold/index.js
+++ src/common/components/Tooltip/Gold/index.js
@@ -1,23 +1,24 @@
 import { PropTypes } from 'react';
 import colours from 'common/styles/colours.less';
 import styles from './styles.less';
+import Icon from 'common/components/Icon';
 
 const Gold = ({ copper, silver, gold }) => (
   <div className={styles.root}>
 
   {!!gold && (
     <span className={`${styles.money} ${colours.gold}`}>
-      {gold}
+      {gold} <Icon name="gold.png" size="micro" />
     </span>)}
 
   {!!silver && (
     <span className={`${styles.money} ${colours.silver}`}>
-      {silver}
+      {silver} <Icon name="silver.png" size="micro" />
     </span>)}
 
   {!!copper && (
     <span className={`${styles.money} ${colours.copper}`}>
-      {copper}
+      {copper} <Icon name="copper.png" size="micro" />
     </span>)}
   </div>
 );
