fc564facb134d25b7296eba8214358520a776093
madou <michael.dougall@mi9.com.au>
2016-08-28 16:14:12 +1000

    Tweaks front page information

--- src/features/Home/components/Introduction/index.js
+++ src/features/Home/components/Introduction/index.js
@@ -1,23 +1,16 @@
 import { PropTypes } from 'react';
 
-import Card from 'common/components/Card';
 import styles from './styles.less';
 import cx from 'classnames';
-import { Link } from 'react-router';
 
 const Introduction = ({ className, ...props }) => (
   <div {...props} className={cx(styles.root, className)}>
     <h2>What is Guild Wars 2 Armory?</h2>
 
-    <Card className={styles.text}>
+    <div className={styles.text}>
       Guild Wars 2 Armory is an opt-in utility that you can use to find,
       view, and share Guild Wars 2 user, character, and guild pages.
-
-      <br /><br />
-
-      Can't find your characters or guilds? You'll need to opt-in first.
-      <Link to="join"> Create an account</Link>, then add your api token(s)!
-    </Card>
+    </div>
   </div>
 );
 
