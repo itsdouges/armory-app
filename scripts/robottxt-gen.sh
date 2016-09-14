5ffac86532181b9f297f939305cdbc4d4c4c4def
madou <michael.dougall@mi9.com.au>
2016-09-14 21:21:10 +1000

    Adds sitemap to robots txt

--- scripts/robottxt-gen.sh
+++ scripts/robottxt-gen.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
   allow)
-    printf "User-agent: *\nDisallow:" > dist/robots.txt;;
+    printf "User-agent: *\nDisallow:\nSitemap: https://api.gw2armory.com/sitemap.xml" > dist/robots.txt;;
   disallow)
     printf "User-agent: *\nDisallow: /" > dist/robots.txt;;
   *)
