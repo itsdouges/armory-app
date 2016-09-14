ff02ecd21938f1afa8597bab5f1f4b30f7382a67
madou <michael.dougall@mi9.com.au>
2016-09-15 08:08:20 +1000

    Fixes deploy bash script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Deploying to prod from master."
   sh scripts/robottxt-gen.sh allow
   npm run deploy -- --env PROD;
-  exit 1;
+  exit 0;
 fi
 
 echo "Deploying to preview from $TRAVIS_BRANCH."
