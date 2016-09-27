8341853a2463d103bf4c5a61f6dec9dcca25b69b
madou <michael.dougall@mi9.com.au>
2016-09-27 20:52:59 +1000

    Fixes amulet empty name

--- src/common/components/Tooltip/Amulet/index.js
+++ src/common/components/Tooltip/Amulet/index.js
@@ -7,17 +7,17 @@ import SimpleTooltip from '../Simple';
 import Background from '../Background';
 import ItemHeader from '../ItemHeader';
 
-const AmuletTooltip = ({ data: { item: { name, icon, attributes, details, rarity } } }) => {
-  if (!name) {
-    return <Background><SimpleTooltip data="Amulet" /></Background>;
+const AmuletTooltip = ({ data: { item, name } }) => {
+  if (!item.name) {
+    return <Background><SimpleTooltip data={name || 'Amulet'} /></Background>;
   }
 
   return (
     <Background>
-      <ItemHeader name={name} icon={icon} rarity={rarity} />
+      <ItemHeader name={item.name} icon={item.icon} rarity={item.rarity} />
 
       <div>
-        {get(details, 'infix_upgrade.buff.description', []).map((description) => (
+        {get(item.details, 'infix_upgrade.buff.description', []).map((description) => (
           <div key={description}>
             {markup(description)}
           </div>
@@ -25,8 +25,8 @@ const AmuletTooltip = ({ data: { item: { name, icon, attributes, details, rarity
       </div>
 
       <div className={colours.green}>
-        {attributes && Object
-          .entries(attributes)
+        {item.attributes && Object
+          .entries(item.attributes)
           .map(([attrName, value]) =>
             <div key={`${value}${attrName}`}>{`+${value} ${attrName}`}</div>)
         }
@@ -37,6 +37,7 @@ const AmuletTooltip = ({ data: { item: { name, icon, attributes, details, rarity
 
 AmuletTooltip.propTypes = {
   data: PropTypes.object,
+  name: PropTypes.string,
 };
 
 export default AmuletTooltip;
