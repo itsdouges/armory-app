4936acc3a1d9d4bf8ba3867057cc80d616e40b86
madou <michael.dougall@mi9.com.au>
2016-10-19 00:04:32 +1100

    Fixes linting error

--- src/features/App/index.js
+++ src/features/App/index.js
@@ -60,7 +60,7 @@ class App extends Component {
             alias={this.props.userAlias}
           />
 
-          {/* SMELL */ this.state.smallHeader && <br />}
+          {/* SMELL */this.state.smallHeader && <br />}
           {this.props.children}
           <Footer />
         </div>
