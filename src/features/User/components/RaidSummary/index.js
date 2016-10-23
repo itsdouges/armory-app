209f39e229cd83ba6e2f5f04aa7b9b77e2160905
madou <michael.dougall@mi9.com.au>
2016-10-23 14:26:20 +1100

    Adds simple raid summary, adds missing achievement, updated changelog

--- src/features/User/components/RaidSummary/index.js
+++ src/features/User/components/RaidSummary/index.js
@@ -5,28 +5,34 @@ import Icon from 'common/components/Icon';
 import Summary from 'common/layouts/Summary';
 
 const RAID_KILL_ACHIEVEMENTS = {
-  2654: 'Sv',
-  2667: 'Gors',
-  2659: 'Sab',
-  2826: 'Cm',
-  3017: 'Xr',
-  3014: 'Kc',
+  2654: 'Vg', // Vale Guardian
+  2667: 'Gors', // Gorseval
+  2659: 'Sab',  // Sabetha
+  2826: 'Cm', // Cave Monster
+  3017: 'Xr', // Xera
+  3014: 'Kc', // Keep Construct
+  2836: 'Mt', // Matthias
 };
 
 const TOTAL_KILLS = Object.keys(RAID_KILL_ACHIEVEMENTS).length;
 
-const RaidSummary = ({ userAchievements, className }) => {
+const RaidSummary = ({ userAchievements, className, simple }) => {
   const achievements = userAchievements.filter(
     ({ id, done }) => RAID_KILL_ACHIEVEMENTS[id] && done
   );
+
+  const raidKills = `Raid Kills ${achievements.map(({ id }) =>
+    `[${RAID_KILL_ACHIEVEMENTS[id]}]`).join('')}`;
+
+  if (simple) {
+    return <span>{raidKills}</span>;
+  }
 
   return (
     <Summary
       className={className}
       leftIcon={<Icon name="raid.png" size="xlarge" />}
-      title={
-        `Raid Kills ${achievements.map(({ id }) => `[${RAID_KILL_ACHIEVEMENTS[id]}]`).join('')}`
-      }
+      title={raidKills}
       subTitle={
         <ProgressBar
           current={achievements.length}
@@ -43,6 +49,7 @@ RaidSummary.propTypes = {
   className: PropTypes.string,
   achievements: PropTypes.object,
   userAchievements: PropTypes.array,
+  simple: PropTypes.bool,
 };
 
 export default RaidSummary;
