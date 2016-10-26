101d8495f0c8001393ff1b37adf556e1f532124c
madou <michael.dougall@mi9.com.au>
2016-10-27 10:32:53 +1100

    Adds chinese language

--- src/lib/i18n/index.js
+++ src/lib/i18n/index.js
@@ -4,7 +4,7 @@ import * as ls from 'lib/local-storage';
 const LANGUAGE_KEY = 'LANGUAGE_LS_KEY';
 const DEFAULT_LANGUAGE = 'en';
 
-export const languages = ['en', 'fr', 'de', 'es', 'ru'];
+export const languages = ['en', 'fr', 'de', 'es', 'zh', 'ru'];
 
 export function set (lang) {
   ls.set(LANGUAGE_KEY, lang);
