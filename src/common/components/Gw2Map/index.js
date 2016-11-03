df2cfdccab8c96873d8112ce490eefd293f52ac5
madou <michael.dougall@mi9.com.au>
2016-11-03 20:13:38 +1100

    Fixes bad wiki links for i18n

--- src/common/components/Gw2Map/index.js
+++ src/common/components/Gw2Map/index.js
@@ -1,7 +1,20 @@
 import { PropTypes } from 'react';
 import cx from 'classnames';
+import includes from 'lodash/includes';
 
+import * as i18n from 'lib/i18n';
 import styles from './styles.less';
+
+const UNSUPPORTED = ['zh', 'ru'];
+function getWikiSupportedLanguage (language) {
+  if (includes(UNSUPPORTED, language)) {
+    return 'en';
+  }
+
+  return language;
+}
+
+const LANGUAGE = getWikiSupportedLanguage(i18n.get());
 
 const cleanName = (name) => name && name.replace('Beta ', '');
 
@@ -15,7 +28,7 @@ function getStyle (id = 0) {
 
 const Gw2Map = ({ data, className }) => (
   <div className={cx(styles.root, className)} style={getStyle(data.id)}>
-    <a href={`https://wiki.guildwars2.com/wiki/${cleanName(data.name)}`} target="_blank">
+    <a href={`https://wiki-${LANGUAGE}.guildwars2.com/wiki/${cleanName(data.name)}`} target="_blank">
       {data.name && <span title={data.name} className={styles.name}>{data.name}</span>}
     </a>
   </div>
