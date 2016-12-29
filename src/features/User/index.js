918568f9dc3af1110492ce2e1e507683a8cc49b1
Michael Dougall <Michael.Dougall@mi9.com.au>
2016-12-29 11:02:03 +1100

    Adds new flair to guild tab

--- src/features/User/index.js
+++ src/features/User/index.js
@@ -117,6 +117,7 @@ export default class User extends Component {
           {
             to: `/${alias}/guilds`,
             name: T.translate('guilds.name'),
+            flair: 'new',
             content: (
               <ContentCardList
                 noBorder
