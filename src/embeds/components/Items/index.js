4fceb6d0ca9494eff37500e517ade191796e8d1b
Hagai Cohen <hagai.co@gmail.com>
2017-03-13 19:07:06 +0200

    chore(stat-select): final touches and cr notes

--- src/embeds/components/Items/index.js
+++ src/embeds/components/Items/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import type { Items } from 'flowTypes';
+import type { Items, ItemStats } from 'flowTypes';
 
 import { Component } from 'react';
 import { connect } from 'react-redux';
@@ -14,16 +14,16 @@ import styles from './styles.less';
 function mapStateToProps (state) {
   return {
     items: state.items,
-    itemstats: state.itemstats,
+    itemStats: state.itemStats,
   };
 }
 
 type Props = {
   items?: Items,
-  itemstats?: Items,
+  itemStats?: ItemStats,
   fetchItems?: (ids: Array<number>) => void,
   // XXX: Why flow doesn't accept number in ids?
-  fetchItemstats?: (ids: Array<mixed>) => void,
+  fetchItemStats?: (ids: Array<mixed>) => void,
   ids: Array<number>,
   className?: string,
   mode?: 'rune' | 'item',
@@ -32,17 +32,22 @@ type Props = {
 
 @connect(mapStateToProps, {
   fetchItems: actions.fetchItems,
-  fetchItemstats: actions.fetchItemstats,
+  fetchItemStats: actions.fetchItemStats,
 })
 export default class ItemsEmbed extends Component {
   props: Props;
 
-  static renderItem (items: Items, itemstats: Items, id: number, statId?: number, mode?: 'rune' | 'item') {
-    const item = Object.assign({}, items && items[id]);
-    const selectedStat = statId && itemstats && itemstats[statId];
+  static renderItem (id: number, mode?: 'rune' | 'item', statId?: number, items?: Items, itemStats?: ItemStats) {
+    const item = items && items[id];
+    const selectedStat = statId && itemStats && itemStats[statId];
+
+    // no item to render, skip.
+    if (!item) {
+      return undefined;
+    }
 
     // Apply stat on item.
-    if (item && selectedStat) {
+    if (selectedStat && item.details && !item.details.infix_upgrade_applied) {
       const attributes = applyAttributes(item, selectedStat);
 
       item.name = `${selectedStat.name} ${item.name}`;
@@ -50,11 +55,12 @@ export default class ItemsEmbed extends Component {
         id: selectedStat.id,
         attributes,
       };
+      item.details.infix_upgrade_applied = true;
     }
 
     return (<Item
       key={id}
-      item={items && items[id]}
+      item={item}
       name={mode === 'rune' ? 'Rune' : undefined}
       tooltipType={mode === 'rune' ? 'amulets' : undefined}
       className={styles.item}
@@ -62,29 +68,18 @@ export default class ItemsEmbed extends Component {
   }
 
   componentWillMount () {
-    const { ids, statIds, fetchItems, fetchItemstats } = this.props;
+    const { ids, statIds, fetchItems, fetchItemStats } = this.props;
 
     fetchItems && fetchItems(ids);
-    fetchItemstats && fetchItemstats(Object.values(statIds));
+    fetchItemStats && fetchItemStats(Object.values(statIds));
   }
 
   render () {
-    const { ids, statIds, items, itemstats, className, mode } = this.props;
-
-    // Do not render until all items are loaded.
-    const existingItems = items && Object.keys(items);
-    const itemsExists = existingItems && ids.reduce((exists, curId) =>
-      exists && (existingItems.indexOf(curId) > -1),
-    true);
-
-    if (!itemsExists || !items || !itemstats) {
-      /* Use empty placeholder till all items are loaded */
-      return (<div className={className} />);
-    }
+    const { ids, statIds, items, itemStats, className, mode } = this.props;
 
     return (
       <div className={className}>
-        {ids.map((id) => ItemsEmbed.renderItem(items, itemstats, id, statIds[id], mode))}
+        {ids.map((id) => ItemsEmbed.renderItem(id, mode, statIds[id], items, itemStats))}
       </div>
     );
   }
--- src/features/Gw2/itemStats.reducer.js
+++ src/features/Gw2/itemStats.reducer.js
@@ -1,7 +1,7 @@
 import { readItemStats } from 'lib/gw2';
 import createReducer from './reducerFactory';
 
-const { defaultState, reducer } = createReducer('itemstats', readItemStats);
+const { defaultState, reducer } = createReducer('itemStats', readItemStats);
 
 export { defaultState };
 export default reducer;
