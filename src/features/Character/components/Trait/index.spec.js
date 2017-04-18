e96fabad88787e45d057b7b1770e02d33f2fb2cf
Michael Dougall <michael.dougall@domain.com.au>
2017-04-19 09:14:52 +1000

    Adds trait tests

---
+++ src/features/Character/components/Trait/index.spec.js
@@ -0,0 +1,91 @@
+import { shallow } from 'enzyme';
+import { stubComponent, stubStyles } from 'test/utils';
+
+const styles = stubStyles([
+  'root',
+  'active',
+]);
+
+const colours = stubStyles([
+  '_black',
+]);
+
+const TooltipTrigger = stubComponent('TooltipTrigger');
+const Icon = stubComponent('Icon');
+
+const Trait = proxyquire('features/Character/components/Trait', {
+  './styles.less': styles,
+  'common/components/TooltipTrigger': TooltipTrigger,
+  'common/components/Icon': Icon,
+  'common/styles/colours': colours,
+});
+
+describe('<Trait />', () => {
+  describe('<Icon />', () => {
+    it('should set classes', () => {
+      const props = {
+        className: 'neat',
+      };
+
+      const wrapper = shallow(<Trait {...props} />);
+
+      expect(wrapper.find(Icon)).to.have.className(styles.root);
+      expect(wrapper.find(Icon)).to.have.className(props.className);
+    });
+
+    it('should set active class', () => {
+      const props = {
+        active: true,
+      };
+
+      const wrapper = shallow(<Trait {...props} />);
+
+      expect(wrapper.find(Icon)).to.have.className(styles.active);
+    });
+
+    it('should set icon with data', () => {
+      const props = {
+        active: true,
+        data: {
+          icon: 'www.cool.com',
+        },
+      };
+
+      const wrapper = shallow(<Trait {...props} />);
+
+      expect(wrapper.find(Icon)).to.have.prop('src').equal(props.data.icon);
+      expect(wrapper.find(Icon)).to.have.prop('style').eql({
+        backgroundColor: colours._black,
+      });
+    });
+  });
+
+  describe('<TooltipTrigger />', () => {
+    it('should setup tooltip trigger', () => {
+      const props = {
+        data: 'some-cool-data',
+      };
+
+      const wrapper = shallow(<Trait {...props} />);
+
+      expect(wrapper.find(TooltipTrigger).props()).to.contain({
+        type: 'trait',
+        data: props.data,
+      });
+    });
+
+    it('should override tooltip trigger data with text', () => {
+      const props = {
+        data: 'some-cool-data',
+        tooltipTextOverride: 'some-other-data',
+      };
+
+      const wrapper = shallow(<Trait {...props} />);
+
+      expect(wrapper.find(TooltipTrigger).props()).to.contain({
+        type: 'trait',
+        data: props.tooltipTextOverride,
+      });
+    });
+  });
+});
