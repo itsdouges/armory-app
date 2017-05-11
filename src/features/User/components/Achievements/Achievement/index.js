770e74176fc37e2d1f6fce2996eb213f4ed9f89d
Michael Dougall <michael.dougall@domain.com.au>
2017-05-11 14:30:04 +1000

    Achievement bits

--- src/features/User/components/Achievements/Achievement/index.js
+++ src/features/User/components/Achievements/Achievement/index.js
@@ -1,7 +1,8 @@
 // @flow
 
+import { Component } from 'react';
 import T from 'i18n-react';
-
+import cx from 'classnames';
 import Icon from 'common/components/Icon';
 import TooltipTrigger from 'common/components/TooltipTrigger';
 import ProgressBar from 'common/components/ProgressBar';
@@ -10,6 +11,7 @@ import colours from 'common/styles/colours';
 import Money from 'common/components/Tooltip/Gold';
 import Gw2Item from 'common/components/Gw2Item';
 import Gw2Title from 'common/components/Gw2Title';
+import SvgIcon from 'common/components/Icon/Svg';
 
 import styles from './styles.less';
 
@@ -82,76 +84,115 @@ const makeBits = (achievement, userBits = []) => {
     return [];
   }
 
-  return achievement.bits.map((bit, index) => {
-    const completed = userBits.includes(index);
-
-    switch (bit.type) {
-      case 'Text':
-        return bit.text;
-
-      case 'Item':
-        return <Gw2Item id={bit.id} className={!completed && styles.incomplete} size="32" />;
-
-      // Minipet not currently supported.
-      // Skin not currently supported.
-      case 'Minipet':
-      case 'Skin':
-      default:
-        return '';
-    }
-  });
-};
-
-const Achievement = ({ achievement, icon, current, bits }: Props) => {
-  const tier = calculateTier(achievement, current);
-  const name = achievement ? achievement.name : '';
-  const completed = current === tier.count;
-
   return (
-    <Card className={styles.root}>
-      <TooltipTrigger data={{ ...achievement, current, userBits: bits }} type="achievement">
-        <div className={styles.achievementContainer}>
-          <div className={styles.iconContainer}>
-            <Icon size="medium" src={icon} />
+    <ol>
+      {achievement.bits.map((bit, index) => {
+        const completed = userBits.includes(index);
+        let content;
 
-            <ProgressBar
-              backgroundColor="transparent"
-              barColor={colours.achievementBar}
-              max={tier.count || 0}
-              current={current || 0}
-              className={styles.progress}
-              labelClassName={styles.progressLabel}
-              label={completed ? T.translate('words.completed') : ''}
-              vertical
-            />
-          </div>
+        switch (bit.type) {
+          case 'Text':
+            content = bit.text;
+            break;
 
-          <div className={styles.content}>
-            <div className={styles.name}>{name}</div>
-          </div>
-        </div>
-      </TooltipTrigger>
+          case 'Item':
+            content = <Gw2Item id={bit.id} className={!completed && styles.incomplete} size="32" />;
+            break;
 
-      <div className={styles.rewards}>
-        {!!tier.points && (
-          <TooltipTrigger data={T.translate('achievements.pointsThisTier')}>
-            <div className={styles.pointsContainer}>
-              <span className={styles.points}>{tier.points} </span>
-              <Icon sizePx={32} name="arenanet-points.png" />
-            </div>
-          </TooltipTrigger>
-        )}
+          // Minipet not currently supported.
+          // Skin not currently supported.
+          case 'Minipet':
+          case 'Skin':
+          default:
+            return null;
+        }
 
-        {makeRewards(achievement)}
-      </div>
-
-      {makeBits(achievement, bits)}
-    </Card>
+        return (
+          <li key={`${bit.type}-${bit.id || bit.text}`}>
+            <SvgIcon sizePx={16} name={completed ? 'done' : 'clear'} /> {content}
+          </li>
+        );
+      })}
+    </ol>
   );
 };
 
-Achievement.defaultProps = {
-  current: 0,
-};
+export default class Achievement extends Component {
+  props: Props;
 
-export default Achievement;
+  static defaultProps = {
+    current: 0,
+  };
+
+  state: {
+    bitsExpanded: boolean,
+  } = {
+    bitsExpanded: false,
+  };
+
+  toggleBits = () => {
+    this.setState((prevState) => ({
+      bitsExpanded: !prevState.bitsExpanded,
+    }));
+  };
+
+  render () {
+    const { achievement, icon, current, bits } = this.props;
+    const { bitsExpanded } = this.state;
+
+    const tier = calculateTier(achievement, current);
+    const name = achievement ? achievement.name : '';
+    const completed = current === tier.count;
+
+    return (
+      <Card className={styles.root}>
+        <TooltipTrigger data={achievement && { ...achievement, current, userBits: bits }} type="achievement">
+          <div className={styles.achievementContainer}>
+            <div className={styles.iconContainer}>
+              <Icon size="medium" src={icon} />
+
+              <ProgressBar
+                backgroundColor="transparent"
+                barColor={colours.achievementBar}
+                max={tier.count || 0}
+                current={current || 0}
+                className={styles.progress}
+                labelClassName={styles.progressLabel}
+                label={completed ? T.translate('words.completed') : ''}
+                vertical
+              />
+            </div>
+
+            <div className={styles.content}>
+              <div className={styles.name}>{name}</div>
+            </div>
+          </div>
+        </TooltipTrigger>
+
+        <div className={styles.rewards}>
+          {!!tier.points && (
+            <TooltipTrigger data={T.translate('achievements.pointsThisTier')}>
+              <div className={styles.pointsContainer}>
+                <span className={styles.points}>{tier.points} </span>
+                <Icon sizePx={32} name="arenanet-points.png" />
+              </div>
+            </TooltipTrigger>
+          )}
+
+          {makeRewards(achievement)}
+        </div>
+
+        {achievement && achievement.bits && (
+          <button
+            onClick={this.toggleBits}
+            className={cx(styles.bits, {
+              [styles.expanded]: bitsExpanded,
+            })}
+          >
+            {makeBits(achievement, bits)}
+          </button>
+        )}
+      </Card>
+    );
+  }
+}
