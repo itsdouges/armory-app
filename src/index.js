a49438afab099f937790c3b8dd5790bb656b1ff9
Michael Dougall <laheen@gmail.com>
2017-05-25 21:18:46 +1000

    Move base, add comment

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'normalize.css';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import PageView from 'common/components/PageView';
+// Base has to be imported before the app because of the
+// dynamic creation of the redux reducers.
+import Base from './Base';
 import App from 'features/App';
 import authenticatedApp from 'features/Auth/app';
 
-import Base from './Base';
 import history from './history';
 import bootstrapTooltip from 'lib/tooltip';
 import attachFastClick from 'fastclick';
