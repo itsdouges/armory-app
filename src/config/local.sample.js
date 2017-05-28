a3d692b50c09ab6904fbc4a62b2901f0a11bf61d
Michael Dougall <laheen@gmail.com>
2017-05-28 21:43:11 +1000

    fixes styles, updates sample config

--- src/config/local.sample.js
+++ src/config/local.sample.js
@@ -1,11 +1,17 @@
 export default {
+  webUrl: '//localhost:3000',
+
   api: {
-    endpoint: '//api.gw2armory.com/',
-    // endpoint: '//localhost/',
+    // endpoint: '//api.gw2armory.com/',
+    endpoint: '//localhost/',
   },
 
   features: {
     christmas: false,
     ads: false,
+  },
+
+  cache: {
+    saveToLs: false,
   },
 };
