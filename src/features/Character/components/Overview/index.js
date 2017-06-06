bd7fb7f3366d773ef56c8e80756e74e6aa2cfc01
Michael Dougall <laheen@gmail.com>
2017-06-07 00:39:10 +1000

    Remove redundant display ad

--- src/features/Character/components/Overview/index.js
+++ src/features/Character/components/Overview/index.js
@@ -23,7 +23,6 @@ import { selectCharacterMode } from 'features/Character/actions';
 import calculateAttributes from 'lib/gw2/attributes';
 import { leftItems, rightItems } from 'lib/gw2/equipment';
 
-import ResponsiveLeaderboard from 'common/components/DisplayAd/ResponsiveLeaderboard';
 import ImageUpload from 'common/components/ImageUpload';
 import ContentCard from 'common/components/ContentCard';
 
@@ -223,8 +222,6 @@ class CharacterOverview extends Component {
               />)}
           </div>
         </div>}
-
-        <ResponsiveLeaderboard className={styles.ad} />
       </div>
     );
   }
