873817e0605bb47a2713ef10315e67b74ec4fa00
madou <laheen@gmail.com>
2017-09-27 11:51:20 +1000

    chore(deps): ups deps

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,19 @@
-const defaultConfig = require('./default');
+const baseConfig = require('./default');
 
 const configName = __DEVELOPMENT__ ? 'local' : 'prod';
-const overrideConfig = require(`./${configName}`).default;
+
+let environmentConfig;
+
+try {
+  environmentConfig = require(`./${configName}`).default;
+} catch (e) {
+  console.log('>>>>>>>>>>>>>>>>>>>>>');
+  console.log('You\'re missing a local config. Add one to src/config/local.js. Copy local.sample.js if you\'re stuck!');
+  console.log('>>>>>>>>>>>>>>>>>>>>>');
+  throw e;
+}
 
 export default {
-  ...defaultConfig,
-  ...overrideConfig,
+  ...baseConfig,
+  ...environmentConfig,
 };
