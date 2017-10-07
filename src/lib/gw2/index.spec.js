2355228fed74734c07ebd240f84b9c717f8b0ec4
madou <laheen@gmail.com>
2017-10-07 17:01:42 +1100

    chore(tests): adds missing tests

---
+++ src/lib/gw2/index.spec.js
@@ -0,0 +1,42 @@
+import proxyquire from 'proxyquire';
+
+const sandbox = sinon.sandbox.create();
+const post = sandbox.stub();
+
+const { readCalculatedItemStats } = proxyquire.noPreserveCache()('./', {
+  config: {},
+  axios: { post },
+  '../i18n': { get: () => 'en' },
+});
+
+describe('gw2 service', () => {
+  afterEach(() => sandbox.reset());
+
+  describe('calculated item stats', () => {
+    it('should parse response into object of key itemid:statid', async () => {
+      const data = [{
+        id: 1379,
+        name: 'Grieving',
+        attributes: [],
+      }];
+      post.resolves({ data });
+
+      const actual = await readCalculatedItemStats([{
+        id: 1379,
+        itemId: 44444,
+        level: 80,
+        rarity: 'Rare',
+        type: 'Back',
+      }]);
+
+      expect(actual).to.eql({
+        444441379: {
+          id: 1379,
+          itemId: 44444,
+          name: 'Grieving',
+          attributes: [],
+        },
+      });
+    });
+  });
+});
