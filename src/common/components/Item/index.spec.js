eaa2950b7771b6e1c885df9c28db4f1c359385d1
esunder <esunder@gmail.com>
2017-10-15 16:20:01 -0500

    item unit test

---
+++ src/common/components/Item/index.spec.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import proxyquire from 'proxyquire';
+import { stubComponent, stubStyles } from 'test/utils';
+
+const TooltipTrigger = stubComponent('TooltipTrigger');
+const ResourceLink = stubComponent('ResourceLink');
+const Icon = stubComponent('Icon');
+const Gw2Icon = stubComponent('Gw2Icon');
+
+const Item = proxyquire.noCallThru().noPreserveCache()('common/components/Item', {
+  'common/components/TooltipTrigger': TooltipTrigger,
+  'common/components/ResourceLink': ResourceLink,
+  'common/components/Icon': Icon,
+  'common/components/Gw2Icon': Gw2Icon,
+});
+
+
+//import Item from './';
+
+describe('<Item />', () => {
+  it('should default to item tooltipType', () => {
+    const props = {
+      item: { 
+        name: 'myItem'
+      },
+    };
+
+    const wrapper = shallow(<Item {...props} />);
+
+    expect(wrapper.find(TooltipTrigger).first().props()).to.include({
+        type: 'items',
+      });
+
+  });
+
+  it('should set tooltipData for skin when tooltipType is "skins"', () => {
+  });
+});
